d2cd85e8c690580e7feda2995985e6df2127735a
James Brink <brink.james@gmail.com>
2026-03-31 22:56:57 -0700

    docs: link author name to personal website in footer

--- website/.vitepress/config.ts
+++ website/.vitepress/config.ts
@@ -111,7 +111,7 @@ export default defineConfig({
 
     footer: {
       message: 'Released under the MIT License.',
-      copyright: 'Copyright James Brink',
+      copyright: 'Copyright <a href="https://jamesbrink.online/">James Brink</a>',
     },
 
     editLink: {
