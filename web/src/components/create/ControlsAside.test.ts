f1ec53cccbb45ea8d9158a8ed02c3c2d4b421dd8
Jeffrey Dilley <jeff.dilley@gmail.com>
2026-07-25 23:44:25 -0700

    test(web): update host device mocks

--- web/src/components/create/ControlsAside.test.ts
+++ web/src/components/create/ControlsAside.test.ts
@@ -30,6 +30,7 @@ vi.mock("vue-router", () => ({
 vi.mock("../machines/hostClient", () => ({
   hostStatus: () => Promise.reject(new Error("offline in tests")),
   hostModels: () => Promise.reject(new Error("offline in tests")),
+  hostDevices: () => Promise.reject(new Error("offline in tests")),
 }));
 
 function baseForm(
